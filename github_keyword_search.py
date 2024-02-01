6ac0829fcbfe69bd1fb55607018662c38dce9bf9
hectorproko <38566819+hectorproko@users.noreply.github.com>
2024-01-31 23:07:58 -0500

    keyword search working

--- github_keyword_search.py
+++ github_keyword_search.py
@@ -1,8 +1,11 @@
 import requests
+from termcolor import colored
 
-personal_access_token = "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"
+personal_access_token = "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"
+# Base URL for GitHub API
 base_url = "https://api.github.com"
-
+# Define the keyword to search for
+keyword = "cert"
 headers = {"Authorization": f"token {personal_access_token}"}
 
 # Test the connection by getting your user information
@@ -16,3 +19,41 @@ else:
     print("Connection failed with status code:", response.status_code)
 
 
+# Rest of code
+    
+# Get a list of your repositories
+repos_url = f"{base_url}/user/repos"
+headers = {"Authorization": f"token {personal_access_token}"}
+response = requests.get(repos_url, headers=headers)
+repos = response.json()
+
+# Iterate through each repository and search its contents
+for repo in repos:
+    repo_name = repo["name"]
+    print(repo_name)
+    contents_url = f"{base_url}/repos/{repo['owner']['login']}/{repo['name']}/contents"
+    #print(contents_url)
+    response = requests.get(contents_url, headers=headers)
+    contents = response.json()
+
+    for file in contents:
+        if file["type"] == "file":
+            file_url = file["download_url"]
+            #print(file_url)
+            if file_url is not None:
+                response = requests.get(file_url, headers=headers)
+                #print(response)
+                file_contents = response.text
+                if keyword in file_contents:
+                    #print(colored(f"Keyword found in {repo_name}/{file['path']}", "green"))
+                    print(colored(f"Keyword found in {file['path']}", "green"))
+            else:
+                print("\033[91mInvalid URL or URL is None\033[0m")
+                
+
+
+            
+            #if "_links" in file and "git" in file["_links"] and "tree" in file["_links"]["git"]:
+            #    contents_url = file["_links"]["git"]["tree"]
+            #else:
+            #    contents_url = None
